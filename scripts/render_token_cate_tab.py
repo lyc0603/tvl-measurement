8cab8e07a7de1f362c9daf7b918d2c2cf3b8d541
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2023-06-24 20:10:18 +0800

    add ticker + url

--- scripts/render_token_cate_tab.py
+++ scripts/render_token_cate_tab.py
@@ -5,6 +5,10 @@ import pandas as pd
 from scripts.process_token_lst import df_token_cate
 from config.constants import TABLES_PATH
 
+import warnings
+
+warnings.filterwarnings("ignore")
+
 # iterate through unique category in df_token_cate
 for cate in df_token_cate["category"].unique():
     cate_name = cate.lower().replace(" ", "_")
@@ -12,23 +16,25 @@ for cate in df_token_cate["category"].unique():
     # filter df_token_cate by cate
     df_cate = df_token_cate[df_token_cate["category"] == cate]
 
-    # drop category column
-    df_cate = df_cate.drop(columns=["category"])
-
     # if cate is not Stablecoins, drop stable_type column
     if cate != "Stablecoins":
         df_cate = df_cate.drop(columns=["stable_type"])
 
+    # change smart contract column to be a link to etherscan
+    for index, row in df_cate.iterrows():
+        token_address = row["token_address"]
+        symbol = row["symbol"]
+
+        df_cate.loc[
+            index, "symbol"
+        ] = f"\\href{{https://etherscan.io/address/{token_address}}}{{\\tt {symbol}}}"
+
+    # drop category column
+    df_cate = df_cate.drop(columns=["category", "token_address"])
+
     # sort df_cate by name
     df_cate.columns = (
-        ["Name", "Symbol", "Token Address"]
-        if cate != "Stablecoins"
-        else ["Name", "Symbol", "Token Address", "Type"]
-    )
-
-    # change smart contract column to be a link to etherscan
-    df_cate["Token Address"] = df_cate["Token Address"].apply(
-        lambda x: f"\\href{{https://etherscan.io/address/{x}}}{{\\tt {x}}}"
+        ["Name", "Symbol"] if cate != "Stablecoins" else ["Name", "Symbol", "Type"]
     )
 
     # render df_cate
