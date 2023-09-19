264f57f7915a96bac208e0ce066351d3839360b9
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2023-09-19 16:23:47 +0100

    polish figure

--- scripts/plot/plot_total_tvl.py
+++ scripts/plot/plot_total_tvl.py
@@ -3,25 +3,22 @@ Script to plot the total TVL, total TVL without double counting, and TVR of the 
 """
 
 
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
-from config.constants import (
-    CHAIN_LIST,
-    FIGURES_PATH,
-)
-from environ.data_processing.preprocess_tvl import (
-    preprocess_ptc_tvl,
-)
+from config.constants import CHAIN_LIST, FIGURES_PATH
+from environ.data_processing.preprocess_tvl import preprocess_ptc_tvl
 
 PLOT_INFO_DICT = {
-    "tvl": "$TVL$",
-    "totalLiquidityUSD": "$TVL_{DC}$",
-    "tvr": "$TVR$",
+    "tvl": {"label": "$TVL$", "color": "blue"},
+    "totalLiquidityUSD": {"label": "$TVL_{DC}$", "color": "red"},
+    "tvr": {"label": "$TVR$", "color": "black"},
 }
 
 for chain in CHAIN_LIST:
     # set the figure size
-    plt.figure(figsize=(6, 4))
+    plt.figure(figsize=(5, 2))
 
     # load the total tvl data
     df_agg = preprocess_ptc_tvl(
@@ -44,21 +41,44 @@ for chain in CHAIN_LIST:
         ]
 
     # plot the tvl with DC, tvl, and tvr
-    for col, label in PLOT_INFO_DICT.items():
+    for col, info in PLOT_INFO_DICT.items():
         plt.plot(
             df_agg["date"],
             df_agg[col],
-            label=label,
+            label=info["label"],
+            color=info["color"],
+            linewidth=1,
         )
 
-    # show the legend
-    plt.legend()
+    # show the legend on the upper left corner
+    plt.legend(loc="upper left")
+
+    # add the grid and increase the opacity and increase the intensity
+    plt.grid(alpha=0.3)
+
+    # set the unit of the x axis
+    plt.gca().xaxis.set_major_formatter(
+        mdates.DateFormatter("%Y-%m"),
+    )
+    plt.gca().xaxis.set_major_locator(
+        mdates.MonthLocator(interval=2),
+    )
+
+    # label the y axis
+    plt.ylabel("Dollar Amount", fontsize=6)
+
+    # set the unit of the y axis
+    plt.gca().yaxis.get_major_formatter().set_useOffset(False)
+    plt.gca().yaxis.set_major_formatter(
+        plt.FuncFormatter(lambda x, loc: "{:.0f}B".format(x / 1e9))
+    )
+    plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(base=50e9))
 
     # if the chain is total, make the ticks and legend smaller
     if chain == "Total":
-        plt.xticks(fontsize=8)
-        plt.yticks(fontsize=8)
-        plt.legend(prop={"size": 8})
+        plt.xticks(fontsize=6)
+        plt.yticks(fontsize=6)
+        plt.legend(prop={"size": 6})
     else:
         # if the chain is not total, make the ticks and legend bigger
         plt.xticks(fontsize=19)
@@ -66,7 +86,7 @@ for chain in CHAIN_LIST:
         plt.legend(prop={"size": 14})
 
     # rotate the xticks
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=90)
 
     # tight layout
     plt.tight_layout()
